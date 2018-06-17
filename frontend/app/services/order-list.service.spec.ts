84d12658138d1e72d444c5b73f770ce823c8ff16
Vincent.Meng <vincent.meng@deepnetsecurity.com>
2018-06-17 23:07:50 +0100

    service test

--- frontend/app/services/order-list.service.spec.ts
+++ frontend/app/services/order-list.service.spec.ts
@@ -1,15 +1,40 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { OrderListService } from './order-list.service';
+import { Http, HttpModule, XHRBackend, ResponseOptions } from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
+import { mockResponse } from './fake-data';
 
 describe('OrderListService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [OrderListService]
+      imports: [HttpModule],
+      providers: [
+        { provide: XHRBackend, useClass: MockBackend },
+        OrderListService
+      ]
     });
   });
 
-  it('should be created', inject([OrderListService], (service: OrderListService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('should be created and response correct', inject(
+    [OrderListService, XHRBackend],
+    (service: OrderListService, mockBackend) => {
+      expect(service).toBeTruthy();
+
+      mockBackend.connections.subscribe(connection => {
+        connection.mockRespond(
+          new Response(
+            new ResponseOptions({
+              body: JSON.stringify(mockResponse)
+            })
+          )
+        );
+      });
+
+      service.getOrders(1).subscribe(data => {
+        expect(data.error).toEqual(0);
+        expect(data.result.items.length).toEqual(100);
+      });
+    }
+  ));
 });
