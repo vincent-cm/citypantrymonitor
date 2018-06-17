1758cbcb9888e6192204281f299fe76f54a08eb9
Vincent.Meng <vincent.meng@deepnetsecurity.com>
2018-06-17 23:46:28 +0100

    change to 300 maxElement

--- frontend/app/utility/resource.ts
+++ frontend/app/utility/resource.ts
@@ -3,5 +3,5 @@ export const API_URL = 'http://localhost:4300';
 export const scrollEventTimeout = 100;
 export const debounceTimeOut = 1500;
 export const ordersPerPage = 100;
-export const maxElementsInDOM = 500;
+export const maxElementsInDOM = 300;
 export const pageQueueLimit = Math.ceil(maxElementsInDOM / ordersPerPage);
